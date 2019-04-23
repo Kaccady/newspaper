c3f789f7bc1b6979d1d9abb4944ff572bac0e68b
Kaccady <z-rz@mail.com>
2019-04-23 14:26:10 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import pic1 from './1.jpg';
+import pic2 from './2.jpg';
 
+const Column = props => (
+  <div className="column">
+  <h3>Заголовок</h3>
+    <p>
+      текст текст текст текст текст текст текст текст текст текст текст текст
+      текст текст текст текст текст текст текст текст текст текст текст текст
+      текст текст текст текст текст текст текст текст
+    </p>
+    <img className='img' src={props.img}/>
+  </div>
+);
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1>Газета</h1>
+      </div>
+      <div className="main">
+      <Column img={pic1}/>
+      <Column/>
+      <Column img={pic2}/>
+      <Column/>
+      <Column/>
+      <Column/>
+      <Column/>
+      </div>
+      <div className='footer'><h3>Газета, Газета</h3></div>
     </div>
   );
 }
